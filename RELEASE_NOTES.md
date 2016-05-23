d514af68c65ce2ff57d7d971813e28a8528da5c1
Tomas Petricek <tomas@tomasp.net>
2016-05-23 23:58:30 +0100

    Bump version to 0.1.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.1.1 - May 23, 2016
+ * Updated GitHub release
+
 #### 0.1.0 - May 23, 2016
  * Initial release of the FsInteractiveService NuGet package
--- src/FsInteractiveService.Shared/AssemblyInfo.fs
+++ src/FsInteractiveService.Shared/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FsInteractiveService.Shared")>]
 [<assembly: AssemblyProductAttribute("FsInteractiveService")>]
 [<assembly: AssemblyDescriptionAttribute("F# interactive service API exposed via a lightweight HTTP server")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] InformationalVersion = "0.1.1"
