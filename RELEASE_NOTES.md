38fe39f7066d9066a6571e4955497b31fcf9fcc9
Tomas Petricek <tomas@tomasp.net>
2016-05-31 17:34:56 +0100

    Update AddHtmlPrinter docs and release notes

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.2.0 - May 31, 2016
+ * Change AddHtmlPrinter to support FsLab Journal integration
+
 #### 0.1.2 - May 23, 2016
  * Updated GitHub release
 
--- docs/content/htmlprinter.fsx
+++ docs/content/htmlprinter.fsx
@@ -21,18 +21,33 @@ inside an `#if .. #endif` block for compatibility reasons:
 *)
 #if HAS_FSI_ADDHTMLPRINTER
 fsi.AddHtmlPrinter(fun (Table t) ->
-  [ yield "<table>"
-    for i in 0 .. t.GetLength(0)-1 do
-      yield "<tr>"
-      for j in 0 .. t.GetLength(1)-1 do
-        yield "<td>" + t.[i,j] + "</td>" 
-      yield "</tr>"
-    yield "</table>" ] |> String.concat "")
+  let body = 
+    [ yield "<table>"
+      for i in 0 .. t.GetLength(0)-1 do
+        yield "<tr>"
+        for j in 0 .. t.GetLength(1)-1 do
+          yield "<td>" + t.[i,j] + "</td>" 
+        yield "</tr>"
+      yield "</table>" ] 
+    |> String.concat ""
+  seq [ "style", "<style>table { background:#f0f0f0; }</style>" ],
+  body )
 #endif
 (**
-This defines a HTML printer that formats values of type `Table`. Note that this is done based on 
-the type of the function - the `AddHtmlPrinter` method takes a function `'a -> string` and it
-registers a mapping for values of type `'a`.
+This defines a HTML printer that formats values of type `Table`. The function passed to 
+`AddHtmlPrinter` needs to return a value of type `seq<string*string> * string`. This is
+a tuple consisting of two things:
+
+ - The second element is the HTML body that represents the formatted value. Typically,
+   editors will embed this into HTML output.
+
+ - A sequence of key value pairs that represents additional styles and sripts used that
+   are required by the body. The keys can be `style` or `script` (or other custom keys
+   supported by editors) and can be treated in a special way by the editors (e.g. if
+   loading JavaScript dynamically requires placing the HTML content in an `<iframe>`).
+
+Note that this is done based on the type of the function - the `AddHtmlPrinter` 
+method takes a function `'a -> _` and it registers a mapping for values of type `'a`.
 
 You can now define a table as follows:
 *)
@@ -52,4 +67,21 @@ table
 If you are [calling FsInteractiveService via HTTP](http.html), then the formatted HTML will be
 returned as part of the resulting JSON - inside the `html` field of the `details` fields of the
 returned JSON.
+
+Specifying HTML formatting parameters
+-------------------------------------
+
+In addition to the `fsi.AddHtmlPrinter` function, the FsInteractiveService also provides a
+way of specifying HTML formatting parameters. This may be used by editors and libraries to 
+configure how printing of different elements is done.
+
+One parameter that is specified by the FsInteractiveService (with default value `false`) is
+`html-standalone-output`. This specifies whether the produced HTML should be standalone HTML
+that does not require any background service (and can be saved locally). The `false` value
+means that formatters can start background HTTP servers and call them from the formatted HTML
+code (e.g. to load data on the fly). To access the parameter, use:
 *)
+#if HAS_FSI_ADDHTMLPRINTER
+let standaloneHtmlOutput = 
+  fsi.HtmlPrinterParameters.["html-standalone-output"] :?> bool
+#endif
